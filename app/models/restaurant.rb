78098d1e651f7805001d7b7065ec79d34f3efb3a
Benjamin Poucin <bpoucin@gmail.com>
2017-05-17 23:44:52 +0200

    few corrections yelp exercice

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ApplicationRecord
   CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian", "kebab", "fastfood"]
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   validates :name, uniqueness: true, presence: true
   validates :address, presence: true
   validates :category, inclusion: { in: CATEGORIES, allow_nil: false }
